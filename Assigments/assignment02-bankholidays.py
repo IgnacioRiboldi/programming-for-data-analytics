ae3985749436a37694f565918c14894724315622
Ignacio Riboldi <riboldi.ignacio@gmail.com>
2025-10-04 23:32:12 +0100

    Second commitment

--- Assigments/assignment02-bankholidays.py
+++ Assigments/assignment02-bankholidays.py
@@ -8,21 +8,22 @@ url = "https://www.gov.uk/bank-holidays.json"
 response = requests.get(url)
 data = response.json()
 
-# Each region events
-nothernireland_events = data['northern-ireland']['events']
+# Events by region
+northern_ireland_events = data['northern-ireland']['events']
 england_events = data['england-and-wales']['events']
 scotland_events = data['scotland']['events']
 
-# Event dates for other regions
-england_dates = {event['date'] for event in england_events}
-scotland_dates = {event['date'] for event in scotland_events}
+# Titles in other regions
+england_titles = {event['title'] for event in england_events}
+scotland_titles = {event['title'] for event in scotland_events}
 
-# Filter events that are unique to Northern Ireland
-unique_nothernireland_events = [
-    event for event in nothernireland_events
-    if event['date'] not in england_dates and event['date'] not in scotland_dates
-]
+# Filter unique titles
+unique_titles = set()
 
-# Print the unique events
-for event in unique_nothernireland_events:
-    print(f"{event['date']} - {event['title']}")
+for event in northern_ireland_events:
+    title = event['title']
+    if title not in england_titles and title not in scotland_titles:
+        unique_titles.add(title)
+
+for title in unique_titles:
+    print(title)
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # programming-for-data-analytics
 Repository for Programming for data analytics for ATU
+
+# Task 1
+
+# Task 2
+Different sources:
+On below sources I got some examples of different ways I could do this assignment.
+At first I was trying to get unique bank holidays that were not included in other regions than Nothern Ireland, and I was getting as result three times each of them since they had different dates. 
+https://likegeeks.com/unique-values-json-array-python/
+https://www.geeksforgeeks.org/python/python-json-data-filtering/?utm_source=chatgpt.com
+https://www.geeksforgeeks.org/python/python-unique-dictionary-filter-in-list/
+https://stackoverflow.com/questions/42710604/python-select-unique-key-values-from-json-object
+https://realpython.com/python-json/
+
+As I just wanted to show the titles I manage to use the code as in the third link example to finally just have one time titles.
