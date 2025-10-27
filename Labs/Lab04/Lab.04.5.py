cc97414020c24ca2bb50d04c67e77ee37e91c712
Ignacio Riboldi <riboldi.ignacio@gmail.com>
2025-10-27 12:48:31 +0000

    Assigment 05 in progress

--- Labs/Lab04/Lab.04.5.py
+++ Labs/Lab04/Lab.04.5.py
@@ -4,7 +4,7 @@
 import re
 #regex = "\d{1,3}\.\d{1,3} " # this will find other numbers apart from ips
 regex = r"(\d{1,3}\.\d{1,3}\.)\d{1,3}\.\d{1,3}" # we make a group at the beginning to keep
-replacementText = r"\\1XXX.XXX " # note the space at the end to match above
+replacementText = r"\1XXX.XXX " # note the space at the end to match above
 filename = "/Users/nacho/Desktop/University/tutorialdata/www3/access.log"
 outputFileName = "anonymisedIPs.txt"
 with open(filename) as inputFile:
